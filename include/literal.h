2fe45efc67061ae16cc169284ba1fa60f7837b33
sablinkv <k.v.sablin@gmail.com>
2021-03-26 00:57:55 +0500

    Add literals utfw

--- include/literal.h
+++ include/literal.h
@@ -6,12 +6,19 @@ JSONCPP_NAMESPACE_BEGIN
 namespace Literal {
 	std::basic_string<char> operator"" _utf8(const char16_t* Source, size_t Length);
 	std::basic_string<char> operator"" _utf8(const char32_t* Source, size_t Length);
+	std::basic_string<char> operator"" _utf8(const wchar_t* Source, size_t Length);
 
 	std::basic_string<char16_t> operator"" _utf16(const char* Source, size_t Length);
 	std::basic_string<char16_t> operator"" _utf16(const char32_t* Source, size_t Length);
+	std::basic_string<char16_t> operator"" _utf16(const wchar_t* Source, size_t Length);
 
 	std::basic_string<char32_t> operator"" _utf32(const char* Source, size_t Length);
 	std::basic_string<char32_t> operator"" _utf32(const char16_t* Source, size_t Length);
+	std::basic_string<char32_t> operator"" _utf32(const wchar_t* Source, size_t Length);
+
+	std::basic_string<wchar_t> operator"" _utfw(const char* Source, size_t Length);
+	std::basic_string<wchar_t> operator"" _utfw(const char16_t* Source, size_t Length);
+	std::basic_string<wchar_t> operator"" _utfw(const char32_t* Source, size_t Length);
 
 	std::shared_ptr<JsonValue> operator"" _json(const char* Src, size_t Length);
 }
--- src/literal.cpp
+++ src/literal.cpp
@@ -7,7 +7,7 @@ using namespace JSONCPP_NAMESPACE;
 std::basic_string<char> Literal::operator""_utf8(const char16_t* Source, size_t Length)
 {
 	std::basic_string<char> u8String;
-	Uft16::Transcode<char>(Source, Source + Length, std::back_inserter(u8String));
+	Utf16::Transcode<char>(Source, Source + Length, std::back_inserter(u8String));
 	return u8String;
 }
 
@@ -15,6 +15,13 @@ std::basic_string<char> Literal::operator""_utf8(const char32_t* Source, size_t 
 {
 	std::basic_string<char> u8String;
 	Utf32::Transcode<char>(Source, Source + Length, std::back_inserter(u8String));
+	return u8String;
+}
+
+std::basic_string<char> Literal::operator""_utf8(const wchar_t* Source, size_t Length)
+{
+	std::basic_string<char> u8String;
+	UtfW::Transcode<char>(Source, Source + Length, std::back_inserter(u8String));
 	return u8String;
 }
 
@@ -32,6 +39,13 @@ std::basic_string<char16_t> Literal::operator""_utf16(const char32_t* Source, si
 	return u16String;
 }
 
+std::basic_string<char16_t> Literal::operator""_utf16(const wchar_t* Source, size_t Length)
+{
+	std::basic_string<char16_t> u16String;
+	UtfW::Transcode<char16_t>(Source, Source + Length, std::back_inserter(u16String));
+	return u16String;
+}
+
 std::basic_string<char32_t> Literal::operator""_utf32(const char* Source, size_t Length)
 {
 	std::basic_string<char32_t> u32String;
@@ -42,8 +56,36 @@ std::basic_string<char32_t> Literal::operator""_utf32(const char* Source, size_t
 std::basic_string<char32_t> Literal::operator""_utf32(const char16_t* Source, size_t Length)
 {
 	std::basic_string<char32_t> u32String;
-	Uft16::Transcode<char32_t>(Source, Source + Length, std::back_inserter(u32String));
+	Utf16::Transcode<char32_t>(Source, Source + Length, std::back_inserter(u32String));
 	return u32String;
+}
+
+std::basic_string<char32_t> Literal::operator""_utf32(const wchar_t* Source, size_t Length)
+{
+	std::basic_string<char32_t> u32String;
+	UtfW::Transcode<char32_t>(Source, Source + Length, std::back_inserter(u32String));
+	return u32String;
+}
+
+std::basic_string<wchar_t> Literal::operator""_utfw(const char* Source, size_t Length)
+{
+	std::basic_string<wchar_t> wString;
+	Utf8::Transcode<wchar_t>(Source, Source + Length, std::back_inserter(wString));
+	return wString;
+}
+
+std::basic_string<wchar_t> Literal::operator""_utfw(const char16_t* Source, size_t Length)
+{
+	std::basic_string<wchar_t> wString;
+	Utf16::Transcode<wchar_t>(Source, Source + Length, std::back_inserter(wString));
+	return wString;
+}
+
+std::basic_string<wchar_t> Literal::operator""_utfw(const char32_t* Source, size_t Length)
+{
+	std::basic_string<wchar_t> wString;
+	Utf32::Transcode<wchar_t>(Source, Source + Length, std::back_inserter(wString));
+	return wString;
 }
 
 std::shared_ptr<JsonValue> Literal::operator""_json(const char* Src, size_t Length)
