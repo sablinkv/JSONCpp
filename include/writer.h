a96efb949733cac174288a5e776e5c4143296bb4
sablinkv <k.v.sablin@gmail.com>
2021-04-01 20:38:44 +0500

    Added JSON serialize

---
+++ include/writer.h
@@ -0,0 +1,79 @@
+#pragma once
+#include "value.h"
+
+JSONCPP_NAMESPACE_BEGIN
+
+class JSON_API JsonWriter
+{
+protected:
+	explicit JsonWriter(uint32_t Indent) : m_Indent(Indent) {}
+
+public:
+	using OStream = std::basic_ostream<char>;
+
+	virtual ~JsonWriter() = default;
+	virtual void Serialize(const JsonValue* Root) const = 0;
+
+protected:
+	void	Write(OStream& Stream, const JsonValue* Root, uint32_t Level = 1) const;
+
+private:
+	void	WriteHex(OStream& Stream, uint32_t CodePoint) const;
+	void	WriteString(OStream& Stream, const std::string& String) const;
+	void	WriteIndent(OStream& Stream, uint32_t Level) const;
+	void	WriteObject(OStream& Stream, const JsonValue* Root, uint32_t Level) const;
+	void	WriteArray(OStream& Stream, const JsonValue* Root, uint32_t Level) const;
+
+private:
+	uint32_t m_Indent;
+};
+
+class JSON_API JsonStreamWriter : public JsonWriter
+{
+protected:
+	explicit JsonStreamWriter(OStream* Stream, uint32_t Indent) : m_Stream(Stream), JsonWriter(Indent) {}
+
+public:
+	virtual ~JsonStreamWriter() = default;
+
+	JSON_NODISCARD static auto Create(OStream* Stream, uint32_t Indent = 2)
+	{
+		return std::unique_ptr<JsonStreamWriter>(new JsonStreamWriter(Stream, Indent));
+	}
+
+	void Serialize(const JsonValue* Root) const override;
+
+private:
+	OStream* m_Stream;
+};
+
+class JSON_API JsonStringWriter : public JsonWriter
+{
+protected:
+	explicit JsonStringWriter(std::string* OutString, uint32_t Indent) : m_OutString(OutString), JsonWriter(Indent) {}
+
+public:
+	virtual ~JsonStringWriter() = default;
+
+	JSON_NODISCARD static auto Create(std::string* OutString, uint32_t Indent = 2)
+	{
+		return std::unique_ptr<JsonStringWriter>(new JsonStringWriter(OutString, Indent));
+	}
+
+	void Serialize(const JsonValue* Root) const override;
+
+private:
+	std::string* m_OutString;
+};
+
+class JSON_API JsonWriterFactory
+{
+public:
+	using OStream = std::basic_ostream<char>;
+	using String = std::basic_string<char>;
+
+	JSON_NODISCARD static auto Create(OStream* OutStream, int32_t Indent = 2)	{	return JsonStreamWriter::Create(OutStream, Indent);	}
+	JSON_NODISCARD static auto Create(String* OutString, int32_t Indent = 2)	{	return JsonStringWriter::Create(OutString, Indent);	}
+};
+
+JSONCPP_NAMESPACE_END
---
+++ src/writer.cpp
@@ -0,0 +1,130 @@
+#include "writer.h"
+#include "utf.h"
+#include <iomanip>
+
+using namespace JSONCPP_NAMESPACE;
+
+void JsonWriter::Write(OStream& Stream, const JsonValue* Root, uint32_t Level) const
+{
+	JSON_ASSERT(Root != nullptr);
+	switch (Root->GetType())
+	{
+	case JsonType::Object:	WriteObject(Stream, Root, Level); break;
+	case JsonType::Array:	WriteArray(Stream, Root, Level); break;
+	case JsonType::Null:	Stream << "null"; break;
+	case JsonType::Boolean: JSON_FALLTHROUGH;
+	case JsonType::Number:	Stream << Root->AsString().c_str(); break;
+	case JsonType::String:	WriteString(Stream, Root->AsString()); break;
+	default: JSON_ASSERT_MESSAGE(true, "Invalid serialization JSON type.");
+	}
+}
+
+void JsonWriter::WriteHex(OStream& Stream, uint32_t CodePoint) const
+{
+	Stream << "\\u" << std::hex << std::setfill('0') << std::setw(4) << (CodePoint & 0xFFFFU);
+}
+
+void JsonWriter::WriteString(OStream& Stream, const std::string& String) const
+{
+	auto First = String.cbegin();
+	auto Last = String.cend();
+
+	Stream << '"';
+	for (; First != Last; ++First)
+	{
+		auto Char = *First;
+		switch (Char)
+		{
+		case '\"': Stream << "\\\""; break;
+		case '\\': Stream << "\\\\"; break;
+		case '\n': Stream << "\\n"; break;
+		case '\t': Stream << "\\t"; break;
+		case '\b': Stream << "\\b"; break;
+		case '\f': Stream << "\\f"; break;
+		case '\r': Stream << "\\r"; break;
+		default:
+		{
+			if (Char >= static_cast<char>(0x20U))
+				Stream << Char;
+			else
+			{
+				uint32_t CodePoint;
+				First = Utf8::Decode(First, Last, CodePoint) - 1;
+				for (; CodePoint > 0; CodePoint >>= 16)
+					WriteHex(Stream, CodePoint);
+			}
+			break;
+		}
+		}
+	}
+	Stream << '"';
+}
+
+void JsonWriter::WriteIndent(OStream& Stream, uint32_t Level) const
+{
+	if (m_Indent > 0)
+	{
+		auto Offset = m_Indent * Level;
+		Stream << std::string(Offset, ' ');
+	}
+}
+
+void JsonWriter::WriteObject(OStream& Stream, const JsonValue* Root, uint32_t Level) const
+{
+	auto Object = Root->AsObject();
+
+	Stream << '{';
+	if (!Object.empty())
+	{
+		Stream << '\n';
+		auto First = Object.cbegin();
+		auto Last = --Object.cend();
+		for (; First != Object.end(); ++First)
+		{
+			WriteIndent(Stream, Level);
+			// Write Identifier
+			WriteString(Stream, First->first);
+			Stream << ':' << ' ';
+			// Write Json value
+			Write(Stream, First->second.get(), Level + 1);
+			if (First != Last)
+				Stream << ',';
+			Stream << '\n';
+		}
+		WriteIndent(Stream, Level - 1);
+	}
+	Stream << '}';
+}
+
+void JsonWriter::WriteArray(OStream& Stream, const JsonValue* Root, uint32_t Level) const
+{
+	auto Array = Root->AsArray();
+
+	Stream << '[';
+	if (!Array.empty())
+	{
+		auto Length = Array.size();
+		auto Last = Length - 1;
+		for (uint32_t Index = 0; Index < Length; ++Index)
+		{
+			Write(Stream, Array[Index].get(), Level + 1);
+			if (Index != Last)
+				Stream << ',' << ' ';
+		}
+	}
+	Stream << ']';
+}
+
+void JsonStreamWriter::Serialize(const JsonValue* Root) const
+{
+	JSON_ASSERT(m_Stream); 
+	JsonWriter::Write(*m_Stream, Root);
+}
+
+void JsonStringWriter::Serialize(const JsonValue* Root) const
+{
+	JSON_ASSERT(m_OutString);
+	std::ostringstream OStream;
+	JsonWriter::Write(OStream, Root);
+	*m_OutString = OStream.str();
+}
